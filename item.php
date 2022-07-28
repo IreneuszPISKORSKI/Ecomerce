e20ba609c3b5453d3de6fd1d4a69dd36c220271e
Irek <ireneusz.piskorski@le-campus-numerique.fr>
2022-07-28 10:05:12 +0200

    2nd

---
+++ item.php
@@ -0,0 +1,21 @@
+<?php
+    $name = "premiere";
+    $price = "1985€";
+    $urlPhoto = "mr-bean.gif";
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+    <body>
+    <h1>
+        <?=$name?>
+    </h1>
+    <h2>
+        <?=$price?>
+    </h2>
+    <img src="Photo/<?=$urlPhoto?>">
+    </body>
+</html>
